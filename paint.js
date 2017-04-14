e516c25a3d13a8812fa9bbbdc26df3bb03e08650
Matthew Hummer <biggreyson@gmail.com>
2017-04-14 11:48:06 -0600

    added menu; new works; more works; save/load developing

--- paint.js
+++ paint.js
@@ -3,16 +3,33 @@ var cctext = document.getElementById('cctext');
 var currentColor;
 
 window.addEventListener('load', startUp);
-pallet.addEventListener('click', changeColor);
-canvas.addEventListener('click', paint);
-canvas.addEventListener('mousedown', startPaint);
-canvas.addEventListener('mouseup', stopPaint);
 
 
 
 function startUp() {
+    pallet.addEventListener('click', changeColor);
+    canvas.addEventListener('click', paint);
+    canvas.addEventListener('mousedown', startPaint);
+    document.addEventListener('mouseup', stopPaint);
+    menu.addEventListener('click', changeMenu);
+    saverb.addEventListener('click', ()=>{
+      saver.style.display = 'block';
+    });
+    loaderb.addEventListener('click', generateLoader);
+    startOver.addEventListener('click', newCanvas);
+    more.addEventListener('click', generateCanvas);
+    cancelSave.addEventListener('click', () => {
+        saver.style.display = 'none';
+    })
+    cancelLoad.addEventListener('click', clearLoadList);
+    saveIt.addEventListener('click', save);
     currentColor = randomColor();
     currentColorShower.style.backgroundColor = currentColor;
+    if (currentColor == 'black') {
+        cctext.style.color = 'white';
+    } else {
+        cctext.style.color = 'black';
+    }
     generateCanvas();
 
 }
@@ -41,11 +58,11 @@ function changeColor(event) {
             currentColor = event.target.id;
         }
         currentColorShower.style.backgroundColor = currentColor;
-        if (currentColor == 'black'){
-          console.log();
-          cctext.style.color = 'white';
-        }else{
-          cctext.style.color = 'black';
+        if (currentColor == 'black') {
+            console.log();
+            cctext.style.color = 'white';
+        } else {
+            cctext.style.color = 'black';
         }
     }
 }
@@ -64,4 +81,76 @@ function startPaint() {
 
 function stopPaint() {
     canvas.removeEventListener('mouseover', paint)
+}
+
+function changeMenu() {
+    if (controlBar.style.display === "") {
+        controlBar.style.display = "none";
+    } else if (controlBar.style.display === "none") {
+        controlBar.style.display = "";
+    }
+}
+
+function save() {
+    saver.style.display = 'block';
+    let random = Math.random();
+    if (saveName.value in localStorage){
+      if(confirm('That save name already exists. Overwrite?')){
+        localStorage.setItem(saveName.value, random);
+      }
+    }else{
+      localStorage.setItem(saveName.value, random);
+    }
+    console.log(localStorage);
+    saver.style.display = 'none';
+}
+
+
+function generateLoader() {
+    loader.style.display = 'block';
+    loadIt.style.display = "";
+    if (localStorage.length === 0) {
+        let thePs = loader.getElementsByTagName('div')[0].getElementsByTagName('p')
+        thePs[0].innerHTML = 'You do not have any saved works of art!';
+        loadIt.style.display = "none";
+
+    } else {
+        loader.getElementsByTagName('div')[0].getElementsByTagName('p')[0].innerHTML = 'Which file?';
+        for (let saves in localStorage) {
+            let newItem = document.createElement('p');
+            newItem.innerHTML = saves;
+            newItem.className = "aSave"
+            loadList.appendChild(newItem);
+        }
+    }
+
+    console.log(localStorage);
+}
+
+function newCanvas() {
+    if (confirm('Are you sure that you want to start over? All unsaved progress will be lost.')) {
+        deleteCanvas();
+        generateCanvas();
+    }
+}
+
+function deleteCanvas() {
+    while (document.getElementById('canvas').getElementsByTagName('tr').length > 0) {
+        let current = document.getElementById('canvas').firstChild
+        document.getElementById('canvas').removeChild(current);
+    }
+}
+
+function clearLoadList(){
+  loader.style.display = 'none';
+  while (loadList.getElementsByClassName('aSave').length>0){
+    let current = document.getElementById('loadList').firstChild
+    // console.log(current);
+    document.getElementById('loadList').removeChild(current);
+  }
+  // let toRemove = loadList.getElementsByClassName('aSave').length;
+  // for (i=0; i< toRemove ;i++){
+  //   loadList.removeChild(loadList.getElementsByClassName('aSave')[i])
+  //   console.log('a save');
+  // }
 }
